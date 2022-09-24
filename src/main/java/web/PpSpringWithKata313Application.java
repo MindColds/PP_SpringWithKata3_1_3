c3e5e93497260f01ec5b1d331e418a4a597902cb
MindColds <105950494+MindColds@users.noreply.github.com>
2022-09-24 22:12:08 +0300

    I've done it!!!

---
+++ src/main/java/web/PpSpringWithKata313Application.java
@@ -0,0 +1,13 @@
+package web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpSpringWithKata313Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpSpringWithKata313Application.class, args);
+	}
+
+}
